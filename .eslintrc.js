479b3ddc9e5591e6dae8ac405ca728741e4d64c9
Attila Polgar <raglopa@gmail.com>
2018-05-31 11:12:08 +0200

    chore(eslint): add eslint to project

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extends: 'standard',
-  globals: { angular: true, $: true, APP_ENV: true, moment: true },
-  rules: {
-    'space-before-function-paren': ['error', 'never']
-  }
-}
