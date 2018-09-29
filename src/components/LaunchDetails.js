ad4392cfbdcfeb9fb7742c404b00022d1327dfe4
Attila Polgar <raglopa@gmail.com>
2018-09-29 16:05:32 +0200

    fix(navigation): allow to go back from launch details screen

--- src/components/LaunchDetails.js
+++ src/components/LaunchDetails.js
@@ -1,20 +1,35 @@
-import React, { Component } from 'react'
-import { ScrollView } from 'react-native'
-import PropTypes from 'prop-types'
-
-import LaunchView from '@components/LaunchView'
+import React, { Component } from "react"
+import { ScrollView } from "react-native"
+import PropTypes from "prop-types"
+import { Header, Left, Button, Icon, Title, Right, Body } from "native-base"
+import LaunchView from "@components/LaunchView"
 
 class LaunchDetails extends Component {
   static navigationOptions = ({ navigation }) => {
-    const data = navigation.getParam('data')
+    const data = navigation.getParam("data")
     return { title: `Mission: ${data.mission_name}` }
+  }
+
+  handleBackPress = () => {
+    this.props.navigation.goBack()
   }
 
   render() {
     const { navigation } = this.props
-    const data = navigation.getParam('data')
+    const data = navigation.getParam("data")
     return (
       <ScrollView>
+        <Header>
+          <Left>
+            <Button transparent onPress={this.handleBackPress}>
+              <Icon name="arrow-back" />
+            </Button>
+          </Left>
+          <Body>
+            <Title>{data.mission_name}</Title>
+          </Body>
+          <Right />
+        </Header>
         <LaunchView data={data} />
       </ScrollView>
     )
@@ -22,7 +37,7 @@ class LaunchDetails extends Component {
 }
 
 LaunchDetails.propTypes = {
-  navigation: PropTypes.object
+  navigation: PropTypes.object,
 }
 
 export default LaunchDetails
